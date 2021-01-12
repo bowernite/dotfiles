45f1b9239d982e24ef2fb585aab3e57fdcb4d34d
Brett Abramczyk <brettabramczyk@northwesternmutual.com>
2021-01-12 16:13:02 -0600

    utils bootstrap

--- bin/utils.sh
+++ bin/utils.sh
@@ -2,10 +2,24 @@
 
 dotfiles_dir=~/src/personal/dotfiles
 
+#######################################
+# "Installs" a dotfile by symlinking it from this directory to $HOME.
+#
+# Arguments:
+#   from_filename: The filename of the file to be symlinked in this directory
+#   [to_filename]: The name of the target/the symlink to create
+#######################################
 install_dotfile() {
-  local name="$1"
-  local from="$dotfiles_dir/$name"
-  local to="$HOME/$name"
+  local from_filename=$1
+  local to_filename=$2
+
+  # Default the new/to filename to the name of the file in this repo
+  if [[ -z $to_filename ]]; then
+    to_filename=$from_filename
+  fi
+
+  local from="$dotfiles_dir/$from_filename"
+  local to="$HOME/$to_filename"
 
   if [ ! -e $to ]; then
     echo "Creating $to ..."
@@ -18,7 +32,7 @@ install_dotfile() {
 ##############################################################
 log() {
     local separator="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    printf "\n\n\n" 
+    printf "\n\n\n"
     echo $separator
     echo "‍💻 $1"
     echo $separator
