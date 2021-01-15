fbf1eaf303b1e78ef82b50618edf552607e8465e
Brett Abramczyk <bjabramczyk@gmail.com>
2021-01-15 13:57:04 -0600

    dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -11,8 +11,8 @@ dockutil --no-restart --add "/System/Applications/Reminders.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/System/Applications/Music.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
-dockutil --no-restart --add "/System/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Alfred 4.app/Contents/Preferences/Alfred Preferences.app"
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
 
 dockutil --add '~/Desktop' --view grid --display folder
 dockutil --add '~/Documents' --view grid --display folder
