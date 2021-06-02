9edfcdf4461f1f3c3ac945fd523af28412372014
Brett Abramczyk <bjabramczyk@gmail.com>
2021-06-02 14:17:19 +0200

    use shfmt to format all sh files

--- bin/finish-defaults-diff.sh
+++ bin/finish-defaults-diff.sh
@@ -4,7 +4,7 @@
 # Finish the process of diffing `defaults` by comparing the "before" and "after" snapshots
 #################################################
 
-defaults read > bin/.out/defaults-after.plist
+defaults read >bin/.out/defaults-after.plist
 
 # Diff with VS Code
 code --diff bin/.out/defaults-before.plist bin/.out/defaults-after.plist
