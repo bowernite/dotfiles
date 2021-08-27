12b347b9af2d3e612f7ff59925f98b8a04529a95
Brett Abramczyk <bjabramczyk@gmail.com>
2021-08-27 09:22:24 +0200

    boot

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,17 +1,28 @@
 # WIP boot script
+# launchctl plist file located at: /Users/brett/Library/LaunchAgents/com.user.loginscript.plist
 
-# https://stackoverflow.com/questions/6442364/running-script-upon-login-mac
-# Alternative: https://stackoverflow.com/questions/6442364/running-script-upon-login-mac/13372744#13372744
+# Created by following instructions here: https://stackoverflow.com/questions/6442364/running-script-upon-login-mac/13372744#13372744
 
 # The `sleep`s are to prevent VS Code from opening them all in one Workspace together (one window instead of separately)
-open -a "Visual Studio Code" ~/Dropbox/wiki
+# -g makes it not go into the foreground, -j opens it hidden. Doesn't seem to work if the app is already open
+open -gj -a "Visual Studio Code" ~/Dropbox/wiki
 sleep 0.5
-open -a "Visual Studio Code" ~/src/personal/dotfiles
+open -gj -a "Visual Studio Code" ~/src/personal/dotfiles
 sleep 0.5
-open -a "Visual Studio Code" ~/src/work/wonder-web
+open -gj -a "Visual Studio Code" ~/src/work/wonder-web
 sleep 0.5
-open -a "Visual Studio Code" ~/src/personal/qmk_firmware
+open -gj -a "Visual Studio Code" ~/src/personal/qmk_firmware
 
-open -a "Trello"
+open -gj -a "Trello"
 
 # Optionally, hide/minimize any necessary windows, or all of them
+
+#####################################################################
+# Notes
+#####################################################################
+
+# For solving "Load failed 5: Input/output error" on BigSur
+# https://developer.apple.com/forums/thread/665661#:~:text=i%20got%20the%20same%20issue%20after%20updating%20to%20bigsur.
+
+# Alternative, using Automator and creating an "App": https://stackoverflow.com/questions/6442364/running-script-upon-login-mac
+## This was way too many files than I wanted to store in my dotfiles, since a `.app` files is basically just a huge directory. So using a shell script with a LaunchAgents that runs it seemed a lot more lightweight and manageable
