5dc18eb52e3adb54f5f3da575316d3cc1b5e690c
Brett <bjabramczyk@gmail.com>
2025-06-17 11:55:00 -0500

    general-utils validation-utils zshrc__git

--- zsh/utils/validation-utils.sh
+++ zsh/utils/validation-utils.sh
@@ -1,3 +1,5 @@
+source ~/src/personal/dotfiles/zsh/utils/general-utils.sh
+
 #####################################################################
 # Validation utils
 #####################################################################
@@ -58,20 +60,6 @@ function show_validation_result() {
     # for component in "${failed_components[@]}"; do
     #   echo -e "• $component"
     # done
-  fi
-}
-
-# change_directory_or_run_command changes to the specified directory and optionally runs a command.
-# It takes at least one parameter:
-# - target_directory: The directory to change to.
-# - Any additional parameters are treated as a command to run in that directory.
-function change_directory_or_run_command() {
-  local target_directory="$1"
-  shift
-  if [ $# -eq 0 ]; then
-    cd "$target_directory"
-  else
-    (cd "$target_directory" && $*)
   fi
 }
 
