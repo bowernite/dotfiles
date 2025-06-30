87580325b53c0e2175fd99ea38aa161ceab8b02f
Brett <bjabramczyk@gmail.com>
2025-06-30 17:00:14 -0500

    restart-karabiner zshrc__aliases

--- bin/global-utils/restart-karabiner.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# (untested)
-function restart_karabiner() {
-  launchctl stop org.pqrs.karabiner.karabiner_console_user_server
-  launchctl start org.pqrs.karabiner.karabiner_console_user_server
-}
